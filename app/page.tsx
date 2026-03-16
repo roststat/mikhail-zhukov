f7abff3cea130fcc72c0baed2f3096c2a8bf8136
roststat <roststat.info@gmail.com>
2026-03-16 14:16:26 +0300

    Fix: serve HTML directly via route handler to preserve JS functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
-import { readFileSync } from "fs";
-import { join } from "path";
-import MikhailSite from "@/components/MikhailSite";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const html = readFileSync(
-    join(process.cwd(), "mikhail-site.html"),
-    "utf-8"
-  );
-
-  // Извлекаем CSS, body content и JS из HTML файла
-  const styleMatch = html.match(/<style>([\s\S]*?)<\/style>/);
-  const css = styleMatch ? styleMatch[1] : "";
-
-  const bodyMatch = html.match(/<body>([\s\S]*?)<\/body>/);
-  const bodyContent = bodyMatch ? bodyMatch[1] : "";
-
-  const scriptMatch = html.match(/<script>([\s\S]*?)<\/script>/);
-  const js = scriptMatch ? scriptMatch[1] : "";
-
-  return <MikhailSite css={css} bodyContent={bodyContent} js={js} />;
+  redirect("/site");
 }
